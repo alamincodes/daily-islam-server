c280beb62dcb3c121147e5b12349fb9907539e70
Md Alamin <mdalamin88248@gmail.com>
2023-03-20 23:06:18 +0600

    update

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 var cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrauicn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrauicn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -34,7 +32,6 @@ async function run() {
       const duya = req.body;
       const result = await ramadanPayers.insertOne(duya);
       res.send(result);
-      console.log(duya);
     });
   } finally {
   }
